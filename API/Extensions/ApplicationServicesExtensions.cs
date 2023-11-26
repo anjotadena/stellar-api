329ba1ccd9692588c2ad5e1c61f02d7840ab471c
Anjo Tadena <tadena.anjo@gmail.com>
2023-11-27 07:57:55 +0800

    Cleaning up Program class

---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,49 @@
+using API.Errors;
+using Core.Interfaces;
+using Core.Specifications;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddScoped(typeof(ISpecification<>), typeof(BaseSpecification<>));
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        services.AddDbContext<StellarDbContext>(option =>
+        {
+            option.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            // Handle our validation errors
+            options.InvalidModelStateResponseFactory = actionContext =>
+            {
+                var errors = actionContext
+                                        .ModelState
+                                        .Where(e => e.Value.Errors.Count > 0)
+                                        .SelectMany(x => x.Value.Errors)
+                                        .Select(x => x.ErrorMessage)
+                                        .ToArray();
+
+                var errorResponse = new ApiValidationErrorResponse { Errors = errors };
+
+                return new BadRequestObjectResult(errorResponse);
+            };
+        });
+        
+        return services;
+    }
+}
