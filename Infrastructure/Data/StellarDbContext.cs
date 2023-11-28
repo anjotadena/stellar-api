055c10c576d623a6109a8bf10984b57d40fca1e1
Anjo Tadena <tadena.anjo@gmail.com>
2023-11-28 22:38:57 +0800

    Fix sqlite decimal column to support

--- Infrastructure/Data/StellarDbContext.cs
+++ Infrastructure/Data/StellarDbContext.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -20,5 +21,24 @@ public class StellarDbContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        // Specific for sqlite, since it didn't support for decimal let's convert it to double
+        if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+        {
+            Dictionary<string, IEnumerable<PropertyInfo>> entityProperties = new Dictionary<string, IEnumerable<PropertyInfo>>();
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+
+                foreach (var property in properties)
+                {
+                    modelBuilder
+                        .Entity(entityType.Name)
+                        .Property(property.Name)
+                        .HasConversion<double>();
+                }
+            }
+        }
     }
 }
