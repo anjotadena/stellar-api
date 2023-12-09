8368af152d98cbb4b04b94babef11490285f2b07
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-10 07:35:21 +0800

    Implement login

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+using API.DTO;
+using API.Errors;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ApiBaseController
+{
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+
+    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login([FromBody] LoginDto loginDto)
+    {
+        var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+        if (user is null)
+        {
+            return Unauthorized(new ApiResponse(StatusCodes.Status401Unauthorized));
+        }
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if (!result.Succeeded)
+        {
+            return Unauthorized(new ApiResponse(StatusCodes.Status401Unauthorized));
+        }
+
+        return new UserDto
+        {
+            Email = user.Email,
+            Token = "Token",
+            DisplayName = user.DisplayName
+        };
+    }
+}
