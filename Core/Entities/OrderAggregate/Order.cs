096cfdf551c21543d542edfd4d234e593ccebb4c
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-21 08:01:48 +0800

    Create core order aggregate

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,38 @@
+namespace Core.Entities.OrderAggregate;
+
+public class Order
+{
+    public Order()
+    {
+    }
+
+    public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod, decimal subtotal)
+    {
+        BuyerEmail = buyerEmail;
+        ShipToAddress = shipToAddress;
+        DeliveryMethod = deliveryMethod;
+        Subtotal = subtotal;
+        OrderItems = orderItems;
+    }
+
+    public string BuyerEmail { get; set; }
+
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+    public Address ShipToAddress { get; set; }
+
+    public DeliveryMethod DeliveryMethod { get; set; }
+
+    public IReadOnlyList<OrderItem> OrderItems { get; set; }
+
+    public decimal Subtotal { get; set; }
+
+    public OrderStatus Status { get; set; }
+
+    public string PaymentIntentId { get; set; }
+
+    public decimal GetTotal()
+    {
+        return Subtotal + DeliveryMethod.Price;
+    }
+}
